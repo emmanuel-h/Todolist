00ce3f4ceec88b13fe2f6d35a496818dc6cd56c8
emmanuel-h <emmanuel-h@mailbox.org>
2026-08-14 13:07:50 +0200

    chore: bump version to 1.1.0 (code 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "fr.mandarine.todolist"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
